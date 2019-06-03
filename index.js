54088bbf4d5a681b5602c7ebbc5ec5254d147686
github username <dekadekadeka@gmail.com>
2019-06-03 16:54:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(array, holiday) {
+    let holidaycards = []
+    for (let i = 0; i < array.length; i++){
+        holidaycards.push(`Thank you, ${array[i]}, for the wonderful ${holiday} gift!`)
+    }
+    return holidaycards;
+}
+
+function countdown(num){
+    while (num > 0) {
+        console.log(num);
+        num -= 1;
+    }
+    console.log(num);
+}
